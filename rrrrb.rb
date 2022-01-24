a5e524502bb04669cd6cf352b592f847d1766dd6
Agustin <82238717+AgU131@users.noreply.github.com>
2022-01-24 13:04:14 -0300

    Update rrrrb.rb

--- rrrrb.rb
+++ rrrrb.rb
@@ -1,5 +1,20 @@
 class Anashe
   def initialize()
-    
   end
 end
+
+class HolaMundo
+	def initialize()
+	end
+	def saluda()
+		arreglo = [1,2,3] 
+		puts arreglo[0]
+		puts arreglo<<"aAaA"
+		print arreglo[-2]
+		print "\n"
+		print arreglo.push("zZzZz")
+	end
+end
+objeto = HolaMundo.new()
+objeto.saluda
+gets()
